f95d0d254f2e1de021fd175aa459773d65b91282
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-22 10:20:43 +0200

    user's profile ok

--- src/components/core/User/User.js
+++ src/components/core/User/User.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ApiContext } from '../../../services/Api';
+import userImg from '../../../assets/user-icon.png';
 
 const User = () => {
+
+  const { profileApiResponse } = useContext(ApiContext);
+
   return (
     <div>
         <h1>USER'S PROFILE</h1>
+        <img src={userImg} alt="user profile img"/>
+        <ul>
+          <li>Username: {profileApiResponse.display_name}</li>
+          <li>Email: {profileApiResponse.email}</li>
+          <li>Your plan details: Musify {profileApiResponse.product}</li>
+          <li>Country: {profileApiResponse.country}</li>
+          <li>Followers: {profileApiResponse.followers.total}</li>
+        </ul>
     </div>
   )
 }
 
-export default User
+export default User;
