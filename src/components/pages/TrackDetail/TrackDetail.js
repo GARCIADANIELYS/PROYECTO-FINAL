c2dff03f312adcd0b052be2ff4cff9fed4d047f7
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-23 12:08:26 +0200

    antes del merge

--- src/components/pages/TrackDetail/TrackDetail.js
+++ src/components/pages/TrackDetail/TrackDetail.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import './TrackDetails.css';
+import './TrackDetail.css';
 import '../../../App.css';
-import {SpinnerCircular} from 'react-spinners';
 import { TbPlayerTrackNext } from 'react-icons/tb';
 import { AiOutlineFire } from 'react-icons/ai';
 import { Si1001Tracklists } from 'react-icons/si';
-import {BsMusicNote} from 'react-icons/bs';
+import { BsMusicNote } from 'react-icons/bs';
 
 const TrackDetail = () => {
     //con useParams me traigo los datos del Id del track
@@ -39,30 +38,42 @@ const TrackDetail = () => {
 
 
     return (
-        <div className='track-details-container'>
+        <div className='track-details'>
+            <img
+                src={trackDetails.album.images[ 0 ].url}
+                alt={trackDetails.album.name}
+                className='track-details-image'
+            />
 
-            <div className='track-details-img-container'>
-                <h1 className='track-details-track'>{trackDetails.name}</h1>
-                <img
-                    src={trackDetails.album.images[ 0 ].url}
-                    alt={trackDetails.album.name}
-                    className='track-details-img'
-                />
-                <h3>Artist: <TbPlayerTrackNext /> {trackDetails.artists[ 0 ].name}</h3>
-            </div>
+            <div className='track-details-names-container'>
+                <h1 className='details-trackname'>{trackDetails.name} <TbPlayerTrackNext /></h1>
+                <h3 className='details-artistname'>Artist: {trackDetails.artists[ 0 ].name}</h3>
+                <div className='details-info-container'>
+                    <div className='each-div-detail'>
+                        <p>Popularity Track Ranking</p>
+                        <p>{trackDetails.popularity}</p>
+                    </div>
+                    <div>
+                        <p>Popularity Track Ranking</p>
+                        <p>{trackDetails.popularity}</p>
+                    </div>
+                    <div>
+                        <p>Popularity Track Ranking</p>
+                        <p>{trackDetails.popularity}</p>
+                    </div>
+                    <div>
+                        <p>Popularity Track Ranking</p>
+                        <p>{trackDetails.popularity}</p>
+                    </div>
 
-            <div className='track-details-info-container'>
-                <ul>
-                    <li><p>Popularidad: {trackDetails.popularity} <AiOutlineFire /></p></li>
-                    <li><p>{trackDetails.album.album_type} {trackDetails.type} <Si1001Tracklists /></p></li>
-                    <li><p>Álbum: "{trackDetails.album.name}"</p></li>
-                    <li><p><BsMusicNote /> {trackDetails.duration_ms} milisegundos</p></li>
-                </ul>
-            </div>
-
-            <div>
-                <p><Link className='artists-redirecto-link' to="/artists">discover new artists</Link></p>
-                <p><Link className='artists-redirecto-link' to="/search">discover new tracks</Link></p>
+                    {/* 
+                    <ul className='details-list'>
+                        <li><p>Popularity ranking: {trackDetails.popularity} <AiOutlineFire /></p></li>
+                        <li><p>{trackDetails.album.album_type} {trackDetails.type} <Si1001Tracklists /></p></li>
+                        <li><p>Álbum: "{trackDetails.album.name}"</p></li>
+                        <li><p><BsMusicNote /> {trackDetails.duration_ms} milisegundos</p></li>
+                    </ul> */}
+                </div>
             </div>
         </div>
     )
