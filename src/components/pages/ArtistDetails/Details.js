391326415ff7de3ea984ea6472009c69718114b4
GARCIADANIELYS <danielysgarcia@outlook.it>
2023-07-25 20:38:16 +0200

    ok

--- src/components/pages/ArtistDetails/Details.js
+++ src/components/pages/ArtistDetails/Details.js
@@ -87,7 +87,7 @@ const Details = () => {
               <div className="albums-artists-conainer" key={album.id}>
                 <Link to={`/album/${album.id}`}>
                   <img
-                    className="img-album-artist"
+                    className="img-album-artist track-card-image"
                     src={album.images[ 0 ].url}
                     alt={album.name}
                   />
